aaa6841d9f9d584c7fcb48fbc8b780d8acda5a47
aleksandranisimov <sanisimov98@gmail.com>
2019-12-19 19:28:39 +0300

    какие-то графики

---
+++ poslednee_slovo_sasha.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(ggridges)
+library(ggrepel)
+setwd('/Users/aleksandranisimov/Documents/GitHub/dh-hackathon-2019-2020-abnormal_distribution')
+poslednee_slovo = read.delim('poslednee_slovo_python_ver.tsv', sep = "\t")
+
+poslednee_slovo <- poslednee_slovo %>%
+  rename(God_rozhdenija = Год.рождения,
+         Za_chto = За.что,
+         Imya = Имя,
+         Kogda = Когда,
+         Kto = Кто,
+         Statja = По.какой.статье,
+         Prof = Профессия,
+         Punishment = Что.получил)
+
+poslednee_slovo %>%
+  distinct() %>%
+  count(God_rozhdenija, Prof, Kogda, Statja, Imya) %>%
+  filter(God_rozhdenija != "н") %>%
+  ggplot(aes(God_rozhdenija,n, fill = Prof)) +
+  geom_col() +
+  labs(x = "Год Рождения",
+       y = "",
+       title = "Сколько раз задерживали людей того или иного года рождения")
